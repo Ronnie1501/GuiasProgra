8ea3ebed46b56cc5743c8480978577e5306a157c
Ronnie1501 <124007445+Ronnie1501@users.noreply.github.com>
2023-09-27 21:42:44 -0600

    .

---
+++ Actividad Asíncrona/Actividad Asíncrona/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string nombre = "Ronnie ";
+        string apellido = "Portillo";
+        int edad = 19;
+        DateTime fechaNacimiento = new DateTime(2004, 1, 15);
+
+        string informacionPersona = "Mi nombre es " + nombre + apellido + "\nTengo: " + edad.ToString() + " años " + "y cumplo el: " + fechaNacimiento.ToString();
+Console.WriteLine(informacionPersona);
+    }
+}
---
+++ Guía1FuncionesMatemáticas/Guía1FuncionesMatemáticas/Program.cs
@@ -0,0 +1,69 @@
+﻿//Ejercicio #1
+
+//Valor Truncado
+double pi1 = Math.PI;
+int Pi_truncado = (int)pi1;
+Console.WriteLine("El Valor truncado de PI es: " + Pi_truncado);
+
+
+//Redondear Arriba
+double pi2 = Math.PI;
+double RAr = Math.Ceiling(pi2);
+Console.WriteLine(" El Valor redondeado hacia arriba de PI es: " + RAr);
+
+//Redondear Abajo
+double pi3 = Math.PI;
+double RAb = Math.Floor(pi3);
+Console.WriteLine("El Valor redondeado hacia abajo de PI es: " + RAb);
+
+//Ejercicio #2
+
+//Redondear
+double e = Math.E;
+Console.WriteLine("Valor redondeado de E: " + Math.Round(e, 10));
+
+
+//Ejercicio 3
+
+//Suma
+
+int suma = 0;
+for (int i = 1; i <= 20; i++)
+{
+    suma += i * i;
+}
+Console.WriteLine(suma);
+
+//Ejercicio 4
+
+//Raíces Cuadradas
+
+Console.WriteLine("---------------Calculadora--------------");
+Console.Write("Valor de a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Valor de b: ");
+double b = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Valor de c: ");
+double c = Convert.ToDouble(Console.ReadLine());
+
+
+double dscrm = (b * b) - (4 * a * c);
+
+if (dscrm > 0)
+{
+    double raiz1 = (-b + Math.Sqrt(dscrm)) / (2 * a);
+    double raiz2 = (-b - Math.Sqrt(dscrm)) / (2 * a);
+    Console.WriteLine($"Las raíces son {raiz1} y {raiz2}");
+}
+else if (dscrm == 0)
+{
+    double raiz = -b / (2 * a);
+    Console.WriteLine($"La raíz doble es {raiz}");
+}
+else
+{
+    Console.WriteLine("Las raíces no se pueden calcular");
+}
+Console.ReadLine();
