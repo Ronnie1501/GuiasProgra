484f2d6a07b6ae958f6398ea4db55180d83edc66
Ronnie1501 <124007445+Ronnie1501@users.noreply.github.com>
2023-09-29 20:50:00 -0600

    .

---
+++ Guia4FCadenas/Guia4FCadenas/Program.cs
@@ -0,0 +1,70 @@
+﻿//Ejercicio 1
+//Caracteres por tamaño
+
+Console.WriteLine("Ingrese una cadena de caracteres:");
+string cadena = Console.ReadLine();
+if (cadena.Length < 25)
+{
+    cadena = cadena.PadRight(25, '$');
+    Console.WriteLine("La cadena resultante es:");
+    Console.WriteLine(cadena);
+}
+else
+{
+    Console.WriteLine("La cadena es: " + cadena);
+}
+
+//Ejercicio 2
+//Funciones Cadenas
+
+Console.WriteLine("Ingrese una cadena de caracteres:");
+string C1 = Console.ReadLine();
+Console.WriteLine("Ingrese otra cadena de caracteres:");
+string C2 = Console.ReadLine();
+int ResultadoC = C1.CompareTo(C2);
+
+if (ResultadoC == 0)
+{
+    Console.WriteLine("Las cadenas son iguales.");
+}
+else if (ResultadoC < 0)
+{
+    Console.WriteLine("La primera cadena es diferente que la segunda cadena.");
+}
+else
+{
+    Console.WriteLine("La primera cadena es diferente que la segunda cadena.");
+}
+
+bool ResultafoI = C1.Equals(C2);
+
+if (ResultafoI)
+{
+    Console.WriteLine("Las cadenas son iguales.");
+}
+else
+{
+    Console.WriteLine("Las cadenas no son iguales.");
+}
+
+
+// ejercicio 3
+// Convertir, separar y concatenar
+
+string Frase = "MI MAMA ME MIMA, AMO A MI MAMA";
+string FraseMin = Frase.ToLower();
+Console.WriteLine("Frase en minúsculas: " + FraseMin);
+
+string[] Palabras = FraseMin.Split(' ');
+Console.WriteLine("Palabras separadas:");
+
+foreach (string Palabra in Palabras)
+{
+    string PalabraRes = Palabra;
+    if (Palabra.Length < 3)
+    {
+        PalabraRes += "%";
+    }
+
+    Console.WriteLine(PalabraRes);
+}
---
+++ Guía4FFechahora/Guía4FFechahora/Program.cs
@@ -0,0 +1,50 @@
+﻿//Ejercicio 1
+//Arreglo de años
+
+int[] Años = new int[9] { 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 };
+
+Console.WriteLine("Cuenta de años bisiestos:");
+
+foreach (int Año in Años)
+{
+    bool esBisiesto = DateTime.IsLeapYear(Año);
+    string Estado = esBisiesto ? "Bisiesto" : "No bisiesto";
+    Console.WriteLine($"{Año}: {Estado}");
+}
+
+//Ejercicio2 
+//Fecha de Pagos
+
+double MontoP = 3000;
+int PlazoM = 6;
+DateTime Inicio = DateTime.Today;
+
+double Cuota = MontoP / PlazoM;
+
+Console.WriteLine("\n");
+Console.WriteLine("Préstamo:");
+Console.WriteLine("Monto:" + MontoP);
+Console.WriteLine("Plazo en meses:" + PlazoM);
+Console.WriteLine("Cuota:" + Cuota, "\n");
+Console.WriteLine("Fechas de pago y cuotas:");
+for (int i = 0; i < PlazoM; i++)
+{
+    DateTime fechaPago = Inicio.AddMonths(i);
+    Console.WriteLine($"Fecha de pago {i + 1}: {fechaPago.ToShortDateString()} - Cuota: ${Cuota}");
+}
+Console.WriteLine("\n");
+
+//Ejercicio 3
+//Fecha cumpleaños
+
+Console.Write("Ingrese su próxima fecha de cumpleaños en numeros desde el año hasta el dia separados por un guion (YYYY-MM-DD): ");
+string FechaCumpleaños = Console.ReadLine();
+
+if (DateTime.TryParse(FechaCumpleaños, out DateTime fechaCumpleaños))
+{
+    TimeSpan tiempoRestante = fechaCumpleaños - DateTime.Today;
+    int DíasR = tiempoRestante.Days;
+
+    Console.WriteLine($"Fecha de cumpleaños ingresada: {fechaCumpleaños.ToShortDateString()}");
+    Console.WriteLine($"Días restantes hasta el cumpleaños: {DíasR} días");
+}
